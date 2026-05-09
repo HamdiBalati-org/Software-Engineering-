810e32df7d4ab1a525ff2b6aa4b829fff5af769f
AmmarMohammad2002 <s12027945@stu.najah.edu>
2026-05-09 14:50:59 +0300

    Fix BookingsGUI tests

--- src/test/java/com/hamdi/appointments/gui/BookingsGUITest.java
+++ src/test/java/com/hamdi/appointments/gui/BookingsGUITest.java
@@ -1,16 +1,18 @@
-
 package com.hamdi.appointments.gui;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.lang.reflect.Field;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
-import com.hamdi.appointments.gui.BookingsGUI;
-import com.hamdi.appointments.service.AppointmentService;
 import com.hamdi.appointments.repository.AppointmentRepository;
+import com.hamdi.appointments.service.AppointmentService;
 
 public class BookingsGUITest {
 
@@ -26,10 +28,40 @@ public class BookingsGUITest {
     @Test
     void shouldCreateBookingsGUI() throws Exception {
         SwingUtilities.invokeAndWait(() -> {
-            AppointmentService service = new AppointmentService(new AppointmentRepository());
+            AppointmentService service =
+                    new AppointmentService(new AppointmentRepository());
+
             frame = new BookingsGUI(service, "admin");
         });
 
         assertNotNull(frame);
+        assertTrue(frame.isDisplayable());
+        assertEquals("All Bookings - Admin: admin", frame.getTitle());
+    }
+
+    @Test
+    void shouldShowNoBookingsLabelWhenEmpty() throws Exception {
+        SwingUtilities.invokeAndWait(() -> {
+            AppointmentService service =
+                    new AppointmentService(new AppointmentRepository());
+
+            frame = new BookingsGUI(service, "admin");
+
+            JLabel label =
+                    getPrivateField(frame, "noBookingsLabel", JLabel.class);
+
+            assertTrue(label.isVisible());
+        });
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> T getPrivateField(Object object, String fieldName, Class<T> type) {
+        try {
+            Field field = object.getClass().getDeclaredField(fieldName);
+            field.setAccessible(true);
+            return (T) field.get(object);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
